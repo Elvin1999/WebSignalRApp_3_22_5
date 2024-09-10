014b50f4ae2524a40b4904192678cf6822c081ea
Elvin <camalzade_elvin@mail.ru>
2024-09-10 16:40:04 +0400

    Add project files.

---
+++ WebSignalRApp/Hubs/MessageHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WebSignalRApp.Helpers;
+
+namespace WebSignalRApp.Hubs
+{
+    public class MessageHub:Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Others.SendAsync("ReceiveConnectInfo", "User connected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.Others.SendAsync("ReceiveDisconnectInfo", "User disconnected");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message + "'s Offer : ", FileHelper.Read());
+        }
+    }
+}
